f3b09afc0aa660586d01318f027c180ae29420f2
lzxxf <lican823462517@hotmail.com>
2018-10-26 22:44:52 -0500

    Created domain object for user

---
+++ src/com/cl/domain/User.java
@@ -0,0 +1,40 @@
+package com.cl.domain;
+
+public class User {
+
+    private String name;
+    private String password;
+    private int id;
+    private int grade;
+    private String email;
+    private String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/com/cl/service/UserService.java
@@ -0,0 +1,11 @@
+package com.cl.service;
+
+import com.cl.domain.User;
+
+public class UserService {
+
+    //check if user is valid
+    public boolean checkUser(User user) {
+
+    }
+}
